8fdd61e72be5b2bfacf14e88f4373653cec2dc7c
Daryna <condordaryna@gmail.com>
2022-10-08 16:56:41 +0300

    add animations to the page

--- page.js
+++ page.js
@@ -13,6 +13,42 @@ iconMenu.addEventListener("click", function() {
     border.classList.toggle("border");
 });
 
+const article = document.querySelector(".article");
+const authorStudent = document.querySelector(".about-us__authors");
+const btnToTop = document.querySelector(".btn-to-top");
+window.addEventListener('scroll', function() {
+    if (window.scrollY >= document.documentElement.clientHeight * 0.72) {
+      window.requestAnimationFrame(function() {
+          article.classList.add('article__active');
+      });
+    }
+    if (window.scrollY >= document.documentElement.clientHeight * 8.76) {
+        window.requestAnimationFrame(function() {
+            authorStudent.classList.add('about-us__authors__anime');
+        });
+    }
+    if (window.scrollY >= (document.documentElement.scrollHeight - document.documentElement.clientHeight) * 0.4) {
+        window.requestAnimationFrame(function() {
+            btnToTop.classList.add('btn-to-top-show-lg');
+        });
+    }
+    if (window.scrollY <= (document.documentElement.scrollHeight - document.documentElement.clientHeight) * 0.4) {
+        window.requestAnimationFrame(function() {
+            btnToTop.classList.remove('btn-to-top-show-lg');
+        });
+    }
+    if (window.scrollY >= (document.documentElement.scrollHeight - document.documentElement.clientHeight) * 0.89) {
+        window.requestAnimationFrame(function() {
+            btnToTop.classList.add('btn-to-top-show-xs-sm-md');
+        });
+    }
+    if (window.scrollY <= (document.documentElement.scrollHeight - document.documentElement.clientHeight) * 0.89) {
+        window.requestAnimationFrame(function() {
+            btnToTop.classList.remove('btn-to-top-show-xs-sm-md');
+        });
+    }
+});
+
 
 let links = document.querySelectorAll(".menu__link[data-goto]");
 
